e350bd1f665f6fa15456a9593e4420f5064ebf53
santi <robincraft@outlook.co.th>
2026-03-13 00:41:14 +0700

    ml and nn

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,13 +5,13 @@ export default function Home() {
         <div className="bg-gray-700 text-white min-h-screen w-full flex flex-col md:flex-row justify-center items-center gap-10 md:gap-40 font-bold p-10 duration-200">
 
             <Link to="/MachineLearning">
-                <button className="hover:bg-green-500 rounded-xl w-40 h-20 md:w-60 md:h-32 text-sm md:text-xl hover:scale-110">
+                <button className="bg-[#15202b] hover:bg-green-500 rounded-xl w-40 h-20 md:w-60 md:h-32 text-sm md:text-xl hover:scale-110 transition-transform p-4 shadow-lg shadow-black">
                     Machine Learning
                 </button>
             </Link>
 
             <Link to="/NeuralNetwork">
-                <button className="hover:bg-sky-500 rounded-xl w-40 h-20 md:w-60 md:h-32  text-sm md:text-xl hover:scale-110">
+                <button className="bg-[#15202b] hover:bg-sky-500 rounded-xl w-40 h-20 md:w-60 md:h-32 text-sm md:text-xl hover:scale-110 transition-transform p-4 shadow-lg shadow-black">
                     Neural Network
                 </button>
             </Link>
